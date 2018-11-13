6e8cae62f1a0cc3918e176db89732549f52e00e3
Azmi Makarima <amy.azmim@gmail.com>
2018-11-14 03:45:54 +0700

    create shared module

---
+++ src/store/shared.js
@@ -0,0 +1,35 @@
+/* eslint no-param-reassign: off */
+
+export default {
+  state: {
+    loading: false,
+    error: null,
+  },
+  mutations: {
+    setLoading(state, payload) {
+      state.loading = payload;
+    },
+    SET_ERROR(state, payload) {
+      state.error = payload;
+    },
+    CLEAR_ERROR(state) {
+      state.error = null;
+    },
+  },
+  actions: {
+    clearError({ commit }) {
+      commit('CLEAR_ERROR');
+    },
+    setError({ commit }, payload) {
+      commit('SET_ERROR', payload);
+    },
+  },
+  getters: {
+    loading(state) {
+      return state.loading;
+    },
+    error(state) {
+      return state.error;
+    },
+  },
+};
