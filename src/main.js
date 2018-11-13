ed7d79145cef6d3b9fe9e3473c432774606fcab3
Azmi Makarima <amy.azmim@gmail.com>
2018-11-14 03:58:09 +0700

    use bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import BootstrapVue from 'bootstrap-vue';
 import * as firebase from 'firebase';
 import Vue from 'vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import App from './App.vue';
 import router from './router';
@@ -7,6 +11,8 @@ import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
