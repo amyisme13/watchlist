489fb31ee516932d1de7ec8e98492de8ffefb000
Azmi Makarima <amy.azmim@gmail.com>
2018-11-14 03:55:36 +0700

    add firebase init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import * as firebase from 'firebase';
 import Vue from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -9,5 +11,22 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render(h) { return h(App); },
+  render(h) {
+    return h(App);
+  },
+  created() {
+    firebase.initializeApp({
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    });
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.$store.dispatch('autoSignIn', user);
+      }
+    });
+  },
 }).$mount('#app');
