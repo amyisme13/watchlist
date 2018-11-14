3fce7ddbe18f27eece230c1b19c1bbad2d6b49d6
Azmi Makarima <amy.azmim@gmail.com>
2018-11-15 03:45:41 +0700

    add vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import shared from './shared';
 import user from './user';
@@ -11,4 +12,5 @@ export default new Vuex.Store({
     shared,
     user,
   },
+  plugins: [createPersistedState()],
 });
