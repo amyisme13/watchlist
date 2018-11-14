6da5b94a4213377ddabb05f6093ec51a43e742dc
Azmi Makarima <amy.azmim@gmail.com>
2018-11-15 00:56:37 +0700

    create AppError component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
   #app
     AppNavbar
+    AppError
     router-view
 </template>
 <script>
 import AppNavbar from '@/components/AppNavbar.vue';
+import AppError from '@/components/AppError.vue';
 
 export default {
   name: 'App',
   components: {
     AppNavbar,
+    AppError,
   },
 };
 </script>
---
+++ src/components/AppError.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  b-alert(
+    v-if="error",
+    :show="!!error",
+    variant="danger",
+    dismissible,
+    @dismissed="dismiss") {{ error.message }}
+</template>
+
+<script>
+export default {
+  name: 'AppError',
+  computed: {
+    error() {
+      return this.$store.getters.error;
+    },
+  },
+  methods: {
+    dismiss() {
+      this.$store.dispatch('clearError');
+    },
+  },
+};
+</script>
