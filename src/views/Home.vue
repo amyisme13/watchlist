8137dd98e7ad7aa1c33737de78a3e7c30d7d10e3
Azmi Makarima <amy.azmim@gmail.com>
2018-11-15 03:29:18 +0700

    add movie showcases in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,67 @@
 <template lang="pug">
-  h1 Home
+  b-container.p-4.bg-light(fluid)
+    h1 My Watchlist
+    hr
+    h2 Liked
+    MovieShowcase(:movies="liked")
+    h2 Hated
+    MovieShowcase(:movies="hated")
+    h2 Followed
+    MovieShowcase(:movies="followed")
 </template>
 
 <script>
+import db from '@/database';
+
+import MovieShowcase from '@/components/Movie/MovieShowcase.vue';
+
 export default {
   name: 'Home',
+  components: {
+    MovieShowcase,
+  },
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  firestore() {
+    return {
+      movies: db.collection('movies'),
+    };
+  },
+  computed: {
+    liked() {
+      return this.movies
+        .filter(this.rateIs('like'))
+        .sort(this.compareDateBy('createdAt'))
+        .slice(0, 4);
+    },
+    hated() {
+      return this.movies
+        .filter(this.rateIs('hate'))
+        .sort(this.compareDateBy('createdAt'))
+        .slice(0, 4);
+    },
+    followed() {
+      return this.movies
+        .filter(this.rateIs(undefined))
+        .sort(this.compareDateBy('releasedAt', true))
+        .slice(0, 4);
+    },
+  },
+  methods: {
+    rateIs(val) {
+      return ({ rate }) => rate === val;
+    },
+    compareDateBy(field, desc = false) {
+      return (a, b) => {
+        if (desc) {
+          return new Date(b[field]) - new Date(a[field]);
+        }
+        return new Date(a[field]) - new Date(b[field]);
+      };
+    },
+  },
 };
 </script>
