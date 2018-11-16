68db988442241ee44faaf292caac659ffd97f145
Azmi Makarima <amy.azmim@gmail.com>
2018-11-17 02:52:36 +0700

    create MovieCard

---
+++ src/components/Movie/MovieCard.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  b-card(
+    :header="`${movie.title} (${movie.year})`",
+    header-tag="header",
+    :img-src="movie.poster || defaultPoster",
+    :img-alt="`${movie.title} poster`",
+    img-top)
+    b-button(
+      v-if="btnLabel",
+      variant="primary",
+      @click="$emit('click')") {{ btnLabel }}
+</template>
+
+<script>
+export default {
+  name: 'MovieCard',
+  props: ['movie', 'btnLabel'],
+  data() {
+    return {
+      defaultPoster: process.env.VUE_APP_DEFAULT_IMAGE,
+    };
+  },
+};
+</script>
